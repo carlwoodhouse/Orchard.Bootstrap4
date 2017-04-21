2e5faee00c6fa2bc2f0c9881f3bf70b6c9e536b3
Carl Woodhouse <carl.woodhouse@gmail.com>
2017-04-21 22:46:21 +0100

    Added base bootsgtrap4 sass and some views

---
+++ ResourceManifest.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.UI.Resources;
+
+namespace Orchard.Bootstrap {
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            manifest.DefineStyle("Bootstrap.Style").SetUrl("bs.min.css", "bs.css");
+            manifest.DefineScript("Bootstrap.Script").SetUrl("bs.min.js", "bs.js").SetDependencies(new[] { "jQuery" });
+        }
+    }
+}
